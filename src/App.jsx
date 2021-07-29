26a9910ab029a9f0be96d8c2c037bdaaaee17d63
Lukas <lukas.ancikevicius@gmail.com>
2021-07-29 15:47:37 +0300

    bootstrap,axios, categories added, data fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import Categories from './components/Categories';
 
+const apiKey = '32eb8fc7-e83e-482e-96b7-1c7c2835dca6';
+
 class App extends Component {
-  state = {};
+  state = {
+    data: [],
+  };
+
+  componentDidMount() {
+    this.getCategories();
+  }
+
+  getCategories = async () => {
+    try {
+      const allCategories = await axios.get(
+        'https://api.thecatapi.com/v1/categories',
+        {
+          headers: {
+            'x-api-key': apiKey,
+          },
+        }
+      );
+      console.log('allCategories.data', allCategories.data);
+      if (Array.isArray(allCategories.data) && allCategories.data.length) {
+        this.setState({ data: allCategories.data });
+      }
+    } catch (error) {
+      console.warn(error);
+    }
+  };
   render() {
     return (
       <div className="App">
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
+
 class Categories extends Component {
-  state = {
-    data: [],
-  };
   render() {
     return <h1>Cat</h1>;
   }
